f8f6b23706acf04aa6e5c0ee64f8ea2a2bc312be
neel-bp <neel@bitspro.com>
2023-03-27 09:56:01 +0500

    httplogger flag

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 //TODO: flag to control if github commit badge should be updated or not
 func main() {
 	configFileLocation := flag.String("config", "example.toml", "location of config file")
+	httpLogger := flag.Bool("httplog", true, "log http requests (webhook push event and status request)")
 	flag.Parse()
 
 	l := log.New(os.Stdout, "", 0)
@@ -24,10 +25,13 @@ func main() {
 	}
 	r := mux.NewRouter()
 	httpinterface.RouterInit(r)
-	loggedHandler := handlers.LoggingHandler(os.Stdout, r)
+	var handler http.Handler = r
+	if *httpLogger {
+		handler = handlers.LoggingHandler(os.Stdout, handler)
+	}
 
 	srv := &http.Server{
-		Handler: loggedHandler,
+		Handler: handler,
 		Addr:    ":4444",
 	}
 	log.Printf("listening on %s", srv.Addr)
